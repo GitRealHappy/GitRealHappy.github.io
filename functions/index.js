83e2c53ae2d886888762ffc34889176080f302a1
Jess <thegarden@Jesss-Laptop.local>
2024-09-27 09:48:51 -0700

    Trying to fix Subscribe button on events page

--- functions/index.js
+++ functions/index.js
@@ -7,28 +7,23 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 // Initialize Firebase Admin SDK
 admin.initializeApp();
 
-// A function that triggers when a new document is created in the "newsletter-subscribers" collection
+//  triggers when new document is made in "newsletter-subscribers"
 exports.newSubscriberNotification = functions.firestore
-  .document('newsletter-subscribers/{docId}')
-  .onCreate((snap, context) => {
-    const newValue = snap.data(); // The new data from Firestore
-    const email = newValue.email; // Get the email from the document
+    .document("newsletter-subscribers/{docId}")
+    .onCreate((snap, context) => {
+      const newValue = snap.data();
+      const email = newValue.email;
 
-    console.log(`New subscriber: ${email}`);
-
-    // You can add more logic here (like sending an email notification)
-
-    return null;
-  });
+      console.log(`New subscriber: ${email}`);
+      return null;
+    });
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
